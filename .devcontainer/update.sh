d43c1727141dbfc5e5385aeec150b2bea1c5fa19
Patrick Coxall <mr.coxall@mths.ca>
2023-01-17 14:04:22 -0500

    linting fix

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -5,6 +5,11 @@
 # now update the README.md file
 GITHUB_URL=$(git remote get-url origin)
 IFS='/' read -r var1 var2 var3 var4 var5 <<< $GITHUB_URL
+echo "$var1"
+echo "$var2"
+echo "$var3"
+echo "$var4"
+echo "$var5"
 GITHUB_REPO_OWNER=$var4
 GITHUB_REPO_NAME=$var5
 
